a5cb004c0cf0bb930fbc479bd9771588bf8d8f9e
LancerHawk <141824147+Lancerhawk@users.noreply.github.com>
2025-05-04 21:24:20 +0530

    Update Home.jsx

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -53,7 +53,6 @@ const Home = () => {
           <div className="info-icon">🎟️</div>
           <h3>Entry</h3>
           <p>Free for students</p>
-          <p>100rs for visitors</p>
         </div>
       </div>
 
